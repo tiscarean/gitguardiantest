6a4a27b387a3a6fffc77d0cc51c7f2eee33c3819
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-03-17 10:51:03 -0600

    add dangerous info

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const awsKey = 'AKIAYRTGSDFDSTGSHJKT';
   res.render('index', { title: 'Express' });
 });
 
