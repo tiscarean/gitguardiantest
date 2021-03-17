820f56d83530b559acea3d4e409184be6bf83774
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-03-17 11:00:41 -0600

    add slack token

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   const clientId = 'AKIAYRTGSDFDSTGSHJKT';
   const clientSecret = 'f=~er2+cxj&x<*(K}_Dm1e^-U';
+  const slack_token="xoxp-198947049743-7861195093-830655328819-9d40a979cac97bccf1190afb660b37e1";
   res.render('index', { title: 'Express' });
 });
 
