225dd0d76e63636b87841fb3be63f49bf33ac777
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-03-17 11:06:56 -0600

    add aws key i think

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const clientId = 'AKIAYRTGSDFDSTGSHJKT';
+  const clientId = 'AKIAFJKR45SAWSZ5XDF3';
   const clientSecret = 'f=~er2+cxj&x<*(K}_Dm1e^-U';
   const someKey = "xoxp-198947049743-7861195093-830655328819-9d40a979cac97bccf1190afb660b37e1";
   const password = "IHTo=amfWLtJU0!Uz";
