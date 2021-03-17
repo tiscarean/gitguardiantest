76e8182eb91b04b82c7b8bc151cba2dc058843af
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-03-17 11:27:24 -0600

    fix one aws leak

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const clientId = 'AKIAFJKR45SAWSZ5XDF3';
-  const clientSecret = 'f=~er2+cxj&x<*(K}_Dm1e^-U';
+  const clientSecret = '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';
   const credentials = {
-    client_id: A3T6AKIAFJKR45SAWSZ5,
-    client_secret: hjshnk5ex5u34565d4654HJKGjhz545d89sjkjka,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
   };
   const someKey = "xoxp-198947049743-7861195093-830655328819-9d40a979cac97bccf1190afb660b37e1";
   const password = "IHTo=amfWLtJU0!Uz";
