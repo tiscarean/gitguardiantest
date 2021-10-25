0edd8a259a8ca0f21f8232da537043750f357e29
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-10-25 12:05:43 -0500

    remove aws api key from test2

--- test2.js
+++ test2.js
@@ -2,5 +2,5 @@ const someKey = "removed";
 const auth = "removed";
 const pwd = "removed";
 
-sensitiveId = "AKIAX52MPYOTPRUCRC73"
-cardNumber = "4166 6766 6766 6746"
+sensitiveId = "removed"
+cardNumber = "removed"
