8e4ce8193d6303c153072d6f7aafb6eb88d767a2
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-10-25 16:26:27 -0500

    recommit accidentally aws information

--- test2.js
+++ test2.js
@@ -2,5 +2,7 @@ const someKey = "removed";
 const auth = "removed";
 const pwd = "removed";
 
-sensitiveId = "removed"
+
 cardNumber = "removed"
+const sensitiveId = "AKIAX52MPYOTPRUCRC73"
+const sensitiveSecret = " 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";
