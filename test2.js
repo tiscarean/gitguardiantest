cd49212f22601220c300bba3e45d09ccb04b27fc
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-10-25 12:04:01 -0500

    removing secret info from test2

--- test2.js
+++ test2.js
@@ -1,3 +1,4 @@
-const someKey = "xoxp-198947049743-7861195093-830655328819-9d40a979cac97bccf1190afb660b37e1";
-const auth = "IHTo=amfWLtJU0Ut";
-const pwd = "AcE23G+9sf";
+const someKey = "removed";
+const auth = "removed";
+const pwd = "removed";
+
