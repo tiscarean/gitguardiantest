948d1301ca9801e48e670a16b314bdf5a4eb0fea
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-12-03 16:56:00 -0600

    add accidentally sensitive info

--- test2.js
+++ test2.js
@@ -4,3 +4,6 @@ const pwd = "removed";
 
 
 cardNumber = "removed"
+sensitiveId = "AKIAX52MPYOTPRUCRC23"
+sensitiveSecret = " 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"
+cardNumber = "4166 6766 6766 6746"
