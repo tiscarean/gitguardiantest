37ded40de4ff40f225a8b97597aa5a173c9ec0ca
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-12-03 18:00:50 -0600

    add some more sensitive info, accidentally

--- test2.java
+++ test2.java
@@ -1,15 +1,9 @@
-const someKey = "removed";
-const auth = "removed";
-const pwd = "removed";
-
-
-cardNumber = "removed"
-sensitiveId = "AKIAX52MPYOTPRUCRC23"
-sensitiveSecret = " 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"
-cardNumber = "4166 6766 6766 6746"
-
-stripeSensitiveInfo = "sk_test_kovrMB0mupFJXfNZWx6Etg5y"
-githubSensitiveInfo = "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"
-awsAccessKeyId = "AKIA6RIEUTC2YH6NTI5N"
-awsAccessSecretKey = "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"
+class HelloWorld {
+    public static void main(String[] args) {
+        String stripeSensitiveInfo = "sk_test_kovrMB0mupFJXfNZWx6Etg5y"
+		String githubSensitiveInfo = "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"
+		String awsAccessKeyId = "AKIA6RIEUTC2YH6NTI5N"
+		String awsAccessSecretKey = "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"
+    }
+}
  
