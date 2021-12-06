cf8702a931760ccf69b78f161faae2fbdd6d660b
Diego Tiscareño <diego.tiscareno@wizeline.com>
2021-12-06 11:18:03 -0600

    removing sensitive info

--- test2.java
+++ test2.java
@@ -2,8 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         String stripeSensitiveInfo = "sk_test_kovrMB0mupFJXfNZWx6Etg5y";
 		String githubSensitiveInfo = "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";
-		String awsAccessKeyId = "AKIA6RIEUTC2YH6NTI5N";
-		String awsAccessSecretKey = "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";
     }
 }
  
