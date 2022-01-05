4a953f3b5276a6685793586546a1ad3025527b84
Diego Tiscareño <diego.tiscareno@wizeline.com>
2022-01-04 18:45:21 -0600

    add variable named slackToken

--- test2.java
+++ test2.java
@@ -2,6 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         String stripeSensitiveInfo = "sk_test_kovrMB0mupFJXfNZWx6Etg5y";
 		String githubSensitiveInfo = "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";
+        String slackToken = "something";
     }
 }
  
